4a2bb591885e54b0040306052efa3cde512c0b7b
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2024-11-04 16:36:26 -0300

    feat(backend):iniciando servidor para ovir na porta 8080

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
   "fmt"
+  "net/http"
 )
 
+func setupRoutes() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Setup do servidor")
+  })
+}
+
 func main() {
-  fmt.Println("Chat app v0.01")
+  setupRoutes()
+  http.ListenAndServe(":8080", nil)
 }
