2bb270bdcc5f64cb2450c02388dcd279f10ba4c4
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2024-11-07 16:50:14 -0300

    feat(frontend):adicionar componente de cabeçalho

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { connect, sendMsg } from "./api";
+import Header from "./components/Header/Header";
 
 class App extends Component {
   constructor(props) {
@@ -14,11 +15,12 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <button onClick={this.send}>Hit</button>
-      </div>
-    );
+	return (
+		<div className="App">
+			<Header />
+			<button onClick={this.send}>Hit</button>
+		</div>
+	);
   }
 }
 
