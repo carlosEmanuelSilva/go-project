90fd73a2b6df576524608f9837c6dbb19cc107e8
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2024-11-12 13:47:14 -0300

    fix(frontend): corrigindo erro de prop

--- frontend/src/components/Input/Input.jsx
+++ frontend/src/components/Input/Input.jsx
@@ -1,11 +1,11 @@
-import React { Component } from "react";
-import "./Inpust.scss";
+import React, { Component } from "react";
+import "./Input.scss";
 
 class Input extends Component {
 	render() {
 		return (
 			<div className="input">
-				<input onKeyDown={this.prop.send} />
+				<input onKeyDown={this.props.send} />
 			</div>
 		);
 	}
